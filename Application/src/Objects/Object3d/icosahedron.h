3565d6685b590d6ef52a9378b1cf684e5d1436df
xperience89 <maxime.lavergne1@ulaval.ca>
2017-03-11 17:40:49 -0500

    Color, position, scale, rot for the ico

--- Application/src/Objects/Object3d/icosahedron.h
+++ Application/src/Objects/Object3d/icosahedron.h
@@ -8,21 +8,25 @@
 class Icosahedron : public Base3DObject
 {
 public:
-	ofMesh mesh;
-	ofVbo vbo;
-	float ang;
 	Icosahedron();
 	Icosahedron(int x, int y, int z, float radius);
 	~Icosahedron();
 	Icosahedron(int x1, int y1, int x2,int y2, int x3,int y3, int x4,int y4);
 
 	virtual void DrawBoundingBox();
+
+	virtual bool CheckPointCollision(const ofVec3f& mouse, const ofVec3f& objScreenPos);
+
 private:
 	void Setup();
 	void Draw();
-	
 
 	void Load();
+
+	ofMesh mesh;
+	ofVbo vbo;
+	float ang;
+	float radius;
 };
 
 #endif // Icosahedron_H
